0df0be7ab50204d71283f44d6abac794562b7185
Obinna Odirionye <odirionye@gmail.com>
2018-10-07 05:39:24 +0100

    chore(git): added a gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('.');
 
 module.exports = {
   entry: [
@@ -10,13 +11,36 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  
   module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node-modules/, loader: 'babel-loader' },
+      { test: /\.html$/, loader: 'raw-loader', exclude: /node_modules/ },
+      {   test: /\.(css|scss|sass)$/,
+          loader: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: ['css-loader', 'postcss-loader', 'sass-loader']
+          }),
+          exclude: /node_modules/
+      }           
+  ],
     rules: [
       {
         test: /\.exec\.js$/,
         use: [ 'script-loader' ]
       }
     ]
-  }
+  },
+  plugins: [
+    ...
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],            
+        
+      })
+    
+  ]
 
 };
